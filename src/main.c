b0b85feb3f0f2b4c745b83f57570bf8bc281d79a
Kay Grewe <arooen@googlemail.com>
2015-07-29 10:52:28 +0200

    main() taught how to open files and how to panic

--- src/main.c
+++ src/main.c
@@ -1,11 +1,19 @@
+#include <stdlib.h>
 #include "dbg.h"
 
 int main(int argc, char *argv[])
 {
+    FILE *target;
     if(argc < 2)
     {
         log_err("no file specified.");
     }
-    
+
+    target = fopen(argv[1], "r");
+    check(target, "can not open file.");
+
     return 0;
+error:
+    if(target) fclose(target);
+    return -1;
 }
